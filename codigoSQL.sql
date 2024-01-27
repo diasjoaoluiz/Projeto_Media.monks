719e8ad7bc20e595b042178238409d6a0f8fbf16
diasjoaoluiz <joao_dacosta@live.com>
2024-01-26 23:34:22 -0300

    commit final

--- codigoSQL.sql
+++ codigoSQL.sql
@@ -1,20 +1,20 @@
 CREATE TABLE tabelao (
   data TEXT,
-  marca TEXT,
   id_marca INTEGER,
+  marca TEXT,
+  nome_veiculo TEXT,
   vendas INTEGER,
-  valor_do_veiculo INTEGER,
-  nome_veiculo TEXT
+  valor_do_veiculo INTEGER
 );
 
-INSERT INTO tabelao (data, marca, id_marca, vendas, valor_do_veiculo, nome_veiculo)
+INSERT INTO tabelao (data, id_marca, marca, nome_veiculo, vendas, valor_do_veiculo)
 SELECT
+fixedDataBase1.c1,
 fixedDataBase2.c1,
 fixedDataBase2.c2,
-fixedDataBase1.c1,
+fixedDataBase1.c5,
 fixedDataBase1.c3,
-fixedDataBase1.c4,
-fixedDataBase1.c5
+fixedDataBase1.c4
 FROM
 fixedDataBase1 INNER JOIN fixedDataBase2 ON fixedDataBase1.c2 = fixedDataBase2.c1;
 
